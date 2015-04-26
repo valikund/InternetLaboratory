d4d84f2fb807703edaae3f3367e9fe492aabe869
Pásztori Zsolt <valikund@gmail.com>
2015-04-26 15:51:59 +0200

    Képbeolvasás megoldva

--- InternetLaboratory/src/Main.java
+++ InternetLaboratory/src/Main.java
@@ -1,8 +1,12 @@
-
+import org.opencv.core.*;
+import org.opencv.highgui.*;
+ 
 public class Main {
-	public static void Main(String[] args)
-	{
-		String szoveg = "Szia";
-	}
-
-}
+ 
+public static void main(String[] args)
+       {
+System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+   Mat m=Highgui.imread("C:/Users/valikund-pc/Desktop/OpenCV/img.jpg",Highgui.CV_LOAD_IMAGE_COLOR);
+new LoadImage("C:/Users/valikund-pc/Desktop/OpenCV/img.jpg",m);
+       }
+       }
