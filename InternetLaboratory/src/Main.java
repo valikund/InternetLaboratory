a78b843a693eaa27fe92b41229067e680ba52340
Pásztori Zsolt <valikund@gmail.com>
2015-04-26 16:28:50 +0200

    Kép beolvasás

--- InternetLaboratory/src/Main.java
+++ InternetLaboratory/src/Main.java
@@ -7,6 +7,6 @@ public static void main(String[] args)
        {
 System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
    Mat m=Highgui.imread("C:/Users/valikund-pc/Desktop/OpenCV/img.jpg",Highgui.CV_LOAD_IMAGE_COLOR);
-new LoadImage("C:/Users/valikund-pc/Desktop/OpenCV/img.jpg",m);
+   new LoadImage("C:/Users/valikund-pc/Desktop/OpenCV/img.jpg",m);
        }
        }
