d0db25ddc4a4a34fc074a0221e005c03d27374a2
Pásztori Zsolt <valikund@gmail.com>
2015-05-03 14:33:03 +0200

    A google képkeresés már megy de csak 9 elemet dob ki, növelni kellene!

--- InternetLaboratory/src/GoogleAPI/Main.java
+++ InternetLaboratory/src/GoogleAPI/Main.java
@@ -13,26 +13,24 @@ public class Main {
 
 	public static void main(String[] args) throws IOException, IOException {
 		// TODO Auto-generated method stub
-		String google = "http://www.google.com/search?q=";
-		String search = "kutyuli";
-		String charset = "UTF-8";
-		String userAgent = "ExampleBot 1.0 (+http://example.com/bot)"; // Change this to your company's name and bot homepage!
+		String google = "http://www.google.com/search?tbm=isch&q=";
 
-		Elements links = Jsoup.connect(google + URLEncoder.encode(search, charset)).userAgent(userAgent).get().select("li.g>h3>a");
+	    String search = "ufo";
+	    String charset = "UTF-8";
+	    String userAgent = "Chrome/35.0.1916.114";
 
-		for (Element link : links) {
-		    String title = link.text();
-		    String url = link.absUrl("href"); // Google returns URLs in format "http://www.google.com/url?q=<url>&sa=U&ei=<someKey>".
-		    url = URLDecoder.decode(url.substring(url.indexOf('=') + 1, url.indexOf('&')), "UTF-8");
+	    Elements imgTds = Jsoup.connect(google + URLEncoder.encode(search, charset)).userAgent(userAgent).get().select(".images_table>tbody>tr>td");
 
-		    if (!url.startsWith("http")) {
-		        continue; // Ads/news/etc.
-		    }
-
-		    System.out.println("Title: " + title);
-		    System.out.println("URL: " + url);
-		}
-
+	    for (Element td : imgTds) {
+	        //Build in Exception handling at this point just in case you get unexpected input.
+	        System.out.println("Title: "+td.html().split("<br>")[2].split("<br>")[0].replaceAll("<[^>]*>", ""));
+	        System.out.println("Domain: "+td.html().split("<br>")[1].split("<br>")[0].replaceAll("<[^>]*>", ""));
+	        System.out.println("URL (Where the image is embedded): "+td.select("a:has(img)").get(0).attr("href").substring(7).split("&")[0]);
+	        // http:// is left out of the src attribute
+	        System.out.println("Image Preview (gstatic.com): http://"+td.select("a>img").get(0).attr("src").substring(7).split("&")[0]);
+	        System.out.println("-----");
+	        //.substring(7).split("&")[0] Quickest way to get to the URL, but definitely not the best out there.
+	    }
 	}
 
 }
