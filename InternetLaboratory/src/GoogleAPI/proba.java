186f84917df2501a984537654e2e0539a2766591
Pásztori Zsolt <valikund@gmail.com>
2015-05-04 16:50:35 +0200

    Tökéletesen megy az API :D Használjátok egészséggel.

--- InternetLaboratory/src/GoogleAPI/proba.java
+++ InternetLaboratory/src/GoogleAPI/proba.java
@@ -18,33 +18,36 @@ public class proba {
 		// TODO Auto-generated method stub
 		File input = new File("C:/Users/valikund-pc/Desktop/OpenCV/proba.html");
 		PrintWriter output = new PrintWriter(input);
-		int keresesek_szama = 10;
+		int keresesek_szama =13;
+		Integer kereses_pozicioja=0;
 		String[] kimenetiurl = new String[keresesek_szama];
-		
+	
 		String google = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=";
 		
 	    String search = "ufo";
 	    String charset = "UTF-8";
 	    String userAgent = "Chrome/35.0.1916.114";
-		 Document imgTd = Jsoup.connect(google + URLEncoder.encode(search, charset)+"&start=4").userAgent(userAgent).get();
+	    while(kereses_pozicioja < keresesek_szama-1){
+		 Document imgTd = Jsoup.connect(google + URLEncoder.encode(search, charset)+"&start="+kereses_pozicioja).userAgent(userAgent).get();
 			    
 		 output.print(imgTd.text()); 
-		 
 		 String szoveg = imgTd.text();
-		
+		 output.close();
+	        
 		 
-		 int i=0;
+		
 		while(szoveg.contains("unescapedUrl\":") == true){
 		int	 szoveg1 = szoveg.indexOf("unescapedUrl\":");
 		String	resz =szoveg.substring(szoveg1);
 		resz = resz.substring(15);
 		szoveg1 = resz.indexOf("\",");
-		kimenetiurl[i]  = resz.substring(0, szoveg1) ;
+		kimenetiurl[kereses_pozicioja]  = resz.substring(0, szoveg1) ;
 		szoveg = resz.substring(szoveg1);
-		System.out.println(kimenetiurl[i]);
+		System.out.println(kimenetiurl[kereses_pozicioja]);
+		kereses_pozicioja++;
 		}
-        output.close();
-        
+	    }
+       
     
 
 	}
