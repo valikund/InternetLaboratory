56f801e0bb2e834513b1b7838a1263e35597eb5c
Pásztori Zsolt <valikund@gmail.com>
2015-05-04 13:44:12 +0200

    Megcsináltam a szövegfeldolgozót, mostmár kidobja az urlt

--- InternetLaboratory/src/GoogleAPI/proba.java
+++ InternetLaboratory/src/GoogleAPI/proba.java
@@ -2,6 +2,7 @@ package GoogleAPI;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
@@ -17,7 +18,8 @@ public class proba {
 		// TODO Auto-generated method stub
 		File input = new File("C:/Users/valikund-pc/Desktop/OpenCV/proba.html");
 		PrintWriter output = new PrintWriter(input);
-		
+		int keresesek_szama = 10;
+		String[] kimenetiurl = new String[keresesek_szama];
 		
 		String google = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=";
 		
@@ -25,12 +27,25 @@ public class proba {
 	    String charset = "UTF-8";
 	    String userAgent = "Chrome/35.0.1916.114";
 		 Document imgTd = Jsoup.connect(google + URLEncoder.encode(search, charset)+"&start=4").userAgent(userAgent).get();
-	
-		    Elements imgTds = Jsoup.connect(google + URLEncoder.encode(search, charset)).userAgent(userAgent).get().select("['responseData']['results']");
-		    //['responseData']['results'][0]['unescapedUrl'] seelctor statement
-		    
-		 output.print(imgTds.text());  
+			    
+		 output.print(imgTd.text()); 
+		 
+		 String szoveg = imgTd.text();
+		
+		 
+		 int i=0;
+		while(szoveg.contains("unescapedUrl\":") == true){
+		int	 szoveg1 = szoveg.indexOf("unescapedUrl\":");
+		String	resz =szoveg.substring(szoveg1);
+		resz = resz.substring(15);
+		szoveg1 = resz.indexOf("\",");
+		kimenetiurl[i]  = resz.substring(0, szoveg1) ;
+		szoveg = resz.substring(szoveg1);
+		System.out.println(kimenetiurl[i]);
+		}
         output.close();
+        
+    
 
 	}
 
